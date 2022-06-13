d774dcdeb9b6ca4d928de7cc201d56678bcc6ddb
SabinTarba <92083574+SabinTarba@users.noreply.github.com>
2022-06-13 18:19:21 +0300

    Update sdd.c

--- sdd.c
+++ sdd.c
@@ -19,3 +19,52 @@ int *removeDuplicate(int *vector, int *dim)
 
     return vector;
 }
+
+// -------------------------------------------------------------------------------------------------------
+struct NodArb* stergeRad(struct NodArb* rad)
+{
+	struct NodArb* aux = rad;
+	if (rad->left != NULL)
+	{
+		rad = aux->left;
+
+		if (aux->right != NULL)
+		{
+			struct NodArb* maxS = aux->left;
+
+			while (maxS->right != NULL)
+				maxS = maxS->right;
+
+			maxS->right = aux->right;
+		}
+	}
+	else
+		if (aux->right != NULL)
+			rad = aux->right;
+		else
+			rad = NULL;
+
+	free(aux->lib.denumire);
+	free(aux);
+
+	return rad;
+}
+
+// -------------------------------------------------------------------------------------------------------
+
+struct NodArb* stergereNod(struct NodArb* rad, char* cheie)
+{
+	if (rad != NULL)
+	{
+		if (strcmp(cheie, rad->lib.denumire) == -1)
+			rad->left = stergereNod(rad->left, cheie);
+		else
+			if (strcmp(cheie, rad->lib.denumire) == 1)
+				rad->right = stergereNod(rad->right, cheie);
+			else
+				rad = stergeRad(rad);
+		return rad;
+	}
+	else
+		return NULL;
+}
